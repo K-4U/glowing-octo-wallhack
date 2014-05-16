e0eac8632b09c1887d569743e7daa00ca9fe3ccb
K-4U <koen@k-4u.nl>
2014-05-16 13:51:23 +0200

    commit from 05/16/14 13:51:23. 0 deletions, 0 adds, 1 modifications

--- src/k4unl/minecraft/gow/blocks/BlockPortalTeleporter.java
+++ src/k4unl/minecraft/gow/blocks/BlockPortalTeleporter.java
@@ -6,6 +6,7 @@ import k4unl.minecraft.gow.lib.config.Names;
 import k4unl.minecraft.gow.lib.helperClasses.Vector3fMax;
 import k4unl.minecraft.gow.tileEntities.TilePortalTeleporter;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.IBlockAccess;
@@ -63,7 +64,12 @@ public class BlockPortalTeleporter extends GOWBlockRendering {
 	
 	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity){
 		if(!world.isRemote){
-			entity.setLocationAndAngles(x+10, y+10, z+10, entity.rotationYaw, entity.rotationPitch);
+			if(entity instanceof EntityPlayer){
+				((EntityPlayer)entity).setPositionAndUpdate(x+10, y+10, z+10);	
+			}else{
+				entity.setLocationAndAngles(x+10, y+10, z+10, entity.rotationYaw, entity.rotationPitch);
+			}
+			
 		}
 	}
 }
