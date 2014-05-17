f1c4c39a5f0e70d2da8eab1fb7e5d04b3c4add01
K-4U <koen@k-4u.nl>
2014-05-17 17:19:47 +0200

    commit from 05/17/14 17:19:47. 0 deletions, 1 adds, 1 modifications

--- src/k4unl/minecraft/gow/containers/ContainerPortalBase.java
+++ src/k4unl/minecraft/gow/containers/ContainerPortalBase.java
@@ -4,6 +4,8 @@ import k4unl.minecraft.gow.tileEntities.TilePortalBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerPortalBase extends Container {
 
@@ -11,13 +13,32 @@ public class ContainerPortalBase extends Container {
 	
 	public ContainerPortalBase(InventoryPlayer invPlayer, TilePortalBase _base){
 		base = _base;
+		addSlotToContainer(new Slot(_base, 0, 35, 49));
 		
+		bindPlayerInventory(invPlayer);
+	}
+	
+	protected void bindPlayerInventory(InventoryPlayer invPlayer){
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 9; j++){
+				addSlotToContainer(new Slot(invPlayer, j + (i * 9) + 9, 8 + (j * 18), 84 + (i * 18)));
+			}
+		}
 		
+		for(int j = 0; j < 9; j++){
+			addSlotToContainer(new Slot(invPlayer, j, 8 + (j * 18), 142));
+		}
 	}
 	
 	@Override
 	public boolean canInteractWith(EntityPlayer var1) {
 		return false;
+	}
+	
+	
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slot){
+		return null;
 	}
 
 }
